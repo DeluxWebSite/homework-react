a419cfa285bc7c03da0ebb2e8f53d261cda99d3e
Serge_M <sergey.meljnick@yandex.ru>
2022-09-18 20:42:07 +0300

    домашнее задание 2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useRef } from "react";
+
+
 
 function App() {
+  const [state, setState] = useState(0);
+  const ref1 = useRef();
+  const ref2 = useRef();
+  const inputClear = (e) => {
+    ref1.current.value = ''
+    ref2.current.value = ''
+  }
+  const [obj, setObj] = useState([]);
+  const [messageList, setMessageList] = useState([]);
+
+  function change(name, e) {
+    setObj({ ...obj, [name]: e.target.value });
+  }
+  function addMessages() {
+    setMessageList([...messageList, obj]);
+    inputClear();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='App' >
+      <input className='input-style' type="text" name="author" onChange={(e) => change('author', e)} placeholder='author' ref={ref1} />
+      <input className='input-style' type="text" name='message' onChange={(e) => change('message', e)} placeholder='message' ref={ref2} />
+      <button className='btn-input-style' onClick={addMessages} >ОТправить сообщение</button>
+      <hr />
+      {
+        messageList.map((item) => {
+          return (
+            <div className="homework-style">
+              <p>Author : {item.author}</p>
+              <p>Message : {item.message}</p><br />
+            </div>
+          )
+        })
+      }
+      <hr />
+      <div className='counter'>
+        <button onClick={() => setState(state - 1)}>-</button>
+        <h4>{state}</h4>
+        <button onClick={() => setState(state + 1)}>+</button>
+        <button onClick={(() => setState(0))}>Reset</button>
+      </div>
+    </div >
   );
 }
 
